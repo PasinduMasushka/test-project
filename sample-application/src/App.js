9bd73c14ece138098ebbe80180afab1223711aaa
Your Name <you@example.com>
2021-01-25 08:35:54 +0530

    My First Commit

---
+++ sample-application/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+//import MainComponent from './componds/MainComponds';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      
+    </div>
+  );
+}
+
+export default App;
